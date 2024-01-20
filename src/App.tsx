d78f066ea8b9f608556df86c396f8250d4dc65cd
lvasconcellos <lets0402@gmail.com>
2024-01-20 11:30:08 -0300

    Testing Google Tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // import Resume from "./components/Resume";
 import "./assets/css/style.css";
-import "./assets/css/loader.css";
 import Resume from "./components/Resume";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-FVPX9WVXRT");
 
 function App() {
   return <Resume />;
--- src/components/sections/SocialSection.tsx
+++ src/components/sections/SocialSection.tsx
@@ -1,17 +1,30 @@
 import { TFunction } from "i18next";
+import ReactGA from "react-ga";
 
 interface SocialSectionProps {
   locale: TFunction<"global">;
 }
 
 const SocialSection: React.FC<SocialSectionProps> = ({ locale }) => {
+  function handleClick(link: string) {
+    ReactGA.event({
+      category: "Button Click",
+      action: `Clicked on ${link}`,
+      label: "Social Page",
+    });
+  }
+
   return (
     <>
       <section className="social section">
         <h2 className="section-title">{locale("section.social")}</h2>
 
         <div className="social__container bd-grid">
-          <a href={locale("social.0.url")} className="social__link">
+          <a
+            href={locale("social.0.url")}
+            className="social__link"
+            onClick={() => handleClick(locale("social.0.name"))}
+          >
             <i className={locale("social.0.icon") + " social__icon"}></i>{" "}
             {locale("social.0.handler")}
           </a>
